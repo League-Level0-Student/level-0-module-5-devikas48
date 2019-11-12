b6d27c049e1c75a615677e80ea36522bb8eb9619
league <league@imac-13.attlocal.net>
2019-11-11 19:58:34 -0800

    prime or not

--- src/primeornot.java
+++ src/primeornot.java
@@ -1,4 +1,39 @@
+import javax.swing.JOptionPane;
 
 public class primeornot {
-
+public static void main(String[] args) {
+	String question = JOptionPane.showInputDialog("Type a number.");
+	
+	int num = Integer.parseInt(question);
+	
+	for (int i = 0; i < args.length; i++) {
+		if(num % 0 == 0) {
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
