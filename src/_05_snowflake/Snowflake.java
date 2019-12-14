75b7e5380950df3f1f0c9c9b9d75d84c003a7d6d
League Student <league@iMac-20.attlocal.net>
2019-12-13 16:56:38 -0800

    for loop gauntlet

--- src/_05_snowflake/Snowflake.java
+++ src/_05_snowflake/Snowflake.java
@@ -5,56 +5,62 @@ package _05_snowflake;
  */
 
 import java.awt.Color;
-import java.awt.Robot;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-Robot rob = new Robot();
+		Robot rob = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-rob.
+		rob.setX(300);
+		rob.setY(300);
 
 		// #3. Put the robot's pen down
-rob.penDown();
+		rob.penDown();
 
 		// #4. Set the robot’s speed to 5
-rob.setSpeed(5);
+		rob.setSpeed(5);
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
+		for (int i = 0; i < 6; i++) {
 
-			
 			// #6. Set the pen color to black
+			rob.setPenColor(Color.black);
 
-			
 			// #7. move the robot by 50
+			rob.move(50);
 
-			
 			// #8. turn the robot to the right by 60
-			
-			
-			// #12. set the pen color to blue for even numbers of i
+			rob.turn(60);
 
-			
+			// #12. set the pen color to blue for even numbers of i
+			if (i % 2 == 0) {
+				rob.setPenColor(Color.blue);
+			}
+
 			// #13. set the pen color to red for all odd numbers of i
-			
-			
+			else {
+				rob.setPenColor(Color.red);
+			}
+
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
+			for (int j = 0; j < 6; j++) {
+
+				// #10. turn the robot to the left by 60
+				rob.turn(-60);
+
+				// #11. move the robot by 50 (play the start button)
+				rob.move(50);
+			}
+
 			// #14. set the pen color to black
-				
-		
+			rob.setPenColor(Color.black);
+		}
 	}
 
 }
---
+++ src/_06_for_loop_gauntlet/forloopgauntlet.java
@@ -0,0 +1,64 @@
+package _06_for_loop_gauntlet;
+
+public class forloopgauntlet {
+public static void main(String[] args) {
+	for (int i = 0; i < 101; i++) {
+		System.out.println(i);
+	
+	}
+	System.out.println("fkgnldrngrjktnksrjnyk");
+	
+for (int j = 100; j > -1; j--) {
+	System.out.println(j);
+}
+					
+System.out.println("fkgnldrngrjktnksrjnyk");
+
+for (int d = 2; d < 103; d++) {
+	 System.out.println(d);
+}																																																																																																																																																																																																																																																																								
+	
+System.out.println("fkgnldrngrjktnksrjnyk");
+
+for (int h = 1; h < 100; h++) {
+	System.out.println(h);
+}
+	
+System.out.println("fkgnldrngrjktnksrjnyk");
+
+	for (int f = 0; f < 501; f++) {
+		if(f%2==0) {
+			System.out.println("even " + f);
+		} else {
+			System.out.println("odd " + f);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
